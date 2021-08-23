08ba8d78ee54c567f9ced2dd6ec72b27b3822b64
Kohei Nakaji <kobejj@gmail.com>
2021-08-22 23:58:21 -0400

    modified

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,107 +3,137 @@ import Square from './Square';
 import React, { useState } from 'react';
 
 const initialState = () => {
-    return [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null],
-    ]}
+	return [ [ null, null, null ], [ null, null, null ], [ null, null, null ] ];
+};
 
 const Game = () => {
+	const [ gameState, setGameState ] = useState(initialState);
+	const [ nextIsX, setNextIsX ] = useState(true);
+	const [ checkGameover, setCheckGameover ] = useState(false);
+	const [ boardIsFull, setBoardIsFull ] = useState(false);
 
-    const [gameState, setGameState] = useState(initialState);
-    const [nextIsX, setNextIsX] = useState(true)
-    const [checkGameover, setCheckGameover] = useState(false)
+	const onClickReset = () => {
+		// console.log('Game is reset');
+		// console.log(initialState);
+		setGameState(initialState);
+		setCheckGameover(false);
+		setNextIsX(true);
+		setBoardIsFull(false);
+	};
 
+	const gameBoard = gameState.map((row, i) => {
+		return (
+			<div key={i}>
+				{row.map((squareValue, j) => {
+					const gameover = () => {
+						if (
+							gameState[0][0] &&
+							gameState[0][0] === gameState[0][1] &&
+							gameState[0][0] === gameState[0][2]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[1][0] &&
+							gameState[1][0] === gameState[1][1] &&
+							gameState[1][0] === gameState[1][2]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[2][0] &&
+							gameState[2][0] === gameState[2][1] &&
+							gameState[2][0] === gameState[2][2]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[0][0] &&
+							gameState[0][0] === gameState[1][0] &&
+							gameState[0][0] === gameState[2][0]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[0][1] &&
+							gameState[0][1] === gameState[1][1] &&
+							gameState[0][1] === gameState[2][1]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[0][2] &&
+							gameState[0][2] === gameState[1][2] &&
+							gameState[0][2] === gameState[2][2]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[0][0] &&
+							gameState[0][0] === gameState[1][1] &&
+							gameState[0][0] === gameState[2][2]
+						)
+							return setCheckGameover(true);
+						else if (
+							gameState[0][2] &&
+							gameState[0][2] === gameState[1][1] &&
+							gameState[0][2] === gameState[2][0]
+						)
+							return setCheckGameover(true);
+					};
 
+					const boardisfull = () => {
+						if (
+							gameState[0][0] !== null &&
+							gameState[0][1] !== null &&
+							gameState[0][2] !== null &&
+							gameState[1][0] !== null &&
+							gameState[1][1] !== null &&
+							gameState[1][2] !== null &&
+							gameState[2][0] !== null &&
+							gameState[2][1] !== null &&
+							gameState[2][2] !== null
+						)
+							setBoardIsFull(true);
+					};
 
-    const onClickReset = () => {
-        console.log('Game is reset')
-        console.log(initialState)
-        setGameState(initialState)
-        setCheckGameover(false)
-    }
+					const handleClick = () => {
+						// console.log(gameState[i][j]);
+						if (gameState[i][j] === 'X' || gameState[i][j] === 'O') {
+							return;
+						}
 
-    const gameBoard = gameState.map((row, i) => {
-        
-        return (
-            <div key={i}>
-                {
-                    row.map((squareValue, j) => {
+						if (checkGameover === true) {
+							return;
+						}
 
+						console.log(`Click happened ${i}-${j}`);
+						const board = [ ...gameState ];
+						board[i][j] = nextIsX ? 'X' : 'O';
+						// console.log(board[i][j]);
+						setGameState(board);
+						setNextIsX(!nextIsX);
+						gameover();
+						boardisfull();
+					};
 
-                        const handleClick = () => {
-                            console.log(gameState[i][j])
-                            if(gameState[i][j] === 'X' || gameState[i][j] ==='O') {
-                                return
-                            }
-                            console.log(`Click happened ${i}-${j}`)
-                            const board = [...gameState]
-                            board[i][j] = nextIsX ? 'X' : 'O'
-                            console.log(board[i][j])
-                            setGameState(board)
-                            setNextIsX(!nextIsX)
-                            const gameover = () => {
-                                if(board[0][0] && board[0][0] === board[0][1] && board[0][0] === board[0][2])
-                                return setCheckGameover(true)
-                                else if(board[1][0] && board[1][0] === board[1][1] && board[1][0] === board[1][2])
-                                return setCheckGameover(true)
-                                else if(board[2][0] && board[2][0] === board[2][1] && board[2][0] === board[2][2])
-                                return setCheckGameover(true)
-                                else if(board[0][0] && board[0][0] === board[1][0] && board[0][0] === board[2][0])
-                                return setCheckGameover(true)
-                                else if(board[0][1] && board[0][1] === board[1][1] && board[0][1] === board[2][1])
-                                return setCheckGameover(true)
-                                else if(board[0][2] && board[0][2] === board[1][2] && board[0][2] === board[2][2])
-                                return setCheckGameover(true)
-                                else if(board[0][0] && board[0][0] === board[1][1] && board[0][0] === board[2][2])
-                                return setCheckGameover(true)
-                                else if(board[0][2] && board[0][2] === board[1][1] && board[0][2] === board[2][0])
-                                return setCheckGameover(true)
-                            }
-                            gameover()
-                        }
-                        
+					return <Square key={`${i}-${j}`} onClick={handleClick} value={squareValue} />;
+				})}
+			</div>
+		);
+	});
 
-                        return (
-                            <Square
-                                key={`${i}-${j}`}
-                                onClick={handleClick}
-                                value={squareValue}
-                            />
-                        )
-                    })
-                }
-                
-
-            </div>
-
-            
-        )
-    })
-
-    return (
-        
-        <div>
-            <h3>Tic-Tak-Toe: Next Player is {nextIsX ? 'X' : 'O'}</h3>
-            {console.log(checkGameover)}
-        
-            <div>
-                {gameBoard[0]}
-            </div>
-            <br/>
-            <div>
-                {gameBoard[1]}
-            </div>
-            <br/>
-            <div>
-                {gameBoard[2]}
-            </div>
-            <br/>
-            <button onClick={onClickReset}>Reset</button>
-            <h3 className="win">{!checkGameover ? '' : `Player ${!nextIsX ? 'X' : 'O'} won!`}</h3>  
-        </div>
-    )
-}
+	return (
+		<div>
+			<h3>Tic-Tac-Toe: Next Player is {nextIsX ? 'X' : 'O'}</h3>
+			{/* {console.log(checkGameover)} */}
+			{gameBoard.map((gameboard) => [ gameboard ])}
+			<br />
+			{/* {gameBoard[0]}
+			<br />
+			{gameBoard[1]}
+			<br />
+			{gameBoard[2]}
+			<br /> */}
+			<button onClick={onClickReset}>Reset</button>
+			<h3 className="win">{!checkGameover ? '' : `Player ${!nextIsX ? 'X' : 'O'} won!`}</h3>
+			<h3 className="tie">{!boardIsFull && !checkGameover ? '' : `${checkGameover ? '' : 'Game is tie...'}`}</h3>
+		</div>
+	);
+};
 
 export default Game;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,13 @@
 import './Game.css';
 
 const Square = (props) => {
+	return (
+		<div>
+			<button className="square" onClick={props.onClick}>
+				{props.value}
+			</button>
+		</div>
+	);
+};
 
-    return (
-        <div>
-            <button className="square" onClick={props.onClick}>
-                {props.value}
-            </button>
-        </div>
-    )
-}
-
-export default Square
+export default Square;
